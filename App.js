786583ff58a57719407bb459cfea2443d29851b8
Kethan Vegunta <kethan@vegunta.com>
2021-01-01 10:10:09 -0800

    change statusbar to dark

--- App.js
+++ App.js
@@ -22,41 +22,39 @@ export default function App() {
         "Roboto": require("./src/assets/fonts/Roboto-Light.ttf"),
         "Courier": require("./src/assets/fonts/CourierPrime-Regular.ttf"),
     });
-	return (
-        <>
-            <StatusBar style="light" />
-            <NavigationContainer>
-                <Tab.Navigator
-                    independent={true}
-                    screenOptions={({ route }) => ({
-                        tabBarButton: [ "Details" ].includes(route.name) ? () => {return null;}: undefined,
-                        tabBarIcon: ({ focused, color, size }) => {
-                            if (route.name === "Blogs") {
-                                return <Entypo name="text-document" size={size} color={color} />;
-                            } else if (route.name === "Post_Blog") {
-                                return <Entypo name="new-message" size={size} color={color} />;
-                            } else if (route.name === "Login") {
-                                return <SimpleLineIcons name="login" size={size} color={color} />;
-                            } else if (route.name === "Sign_Up") {
-                                return <Feather name="user-plus" size={size} color={color} />;
-                            } else if (route.name === "Logout") {
-                                return <SimpleLineIcons name="logout" size={size} color={color} />;
-                            }
-                        },
-                    })}
-                    tabBarOptions={{
-                        activeTintColor: "tomato",
-                        inactiveTintColor: "gray",
-                    }}>
-                    <Tab.Screen name="Blogs" component={Blogs} initialParams={{ message: "" }} />
-                    <Tab.Screen name="Post_Blog" component={PostBlog} />
-                    <Tab.Screen name="Sign_Up" component={Sign_Up} />
-                    <Tab.Screen name="Login" component={Login} />
-                    <Tab.Screen name="Logout" component={Logout} />
-                    <Tab.Screen name="Details" component={Blog_Info} />
-                </Tab.Navigator>
-                <Toast ref={(ref) => Toast.setRef(ref)} />
-            </NavigationContainer>
-        </>
+	return ( 
+        <NavigationContainer>
+            <Tab.Navigator
+                independent={true}
+                screenOptions={({ route }) => ({
+                    tabBarButton: [ "Details" ].includes(route.name) ? () => {return null;}: undefined,
+                    tabBarIcon: ({ focused, color, size }) => {
+                        if (route.name === "Blogs") {
+                            return <Entypo name="text-document" size={size} color={color} />;
+                        } else if (route.name === "Post_Blog") {
+                            return <Entypo name="new-message" size={size} color={color} />;
+                        } else if (route.name === "Login") {
+                            return <SimpleLineIcons name="login" size={size} color={color} />;
+                        } else if (route.name === "Sign_Up") {
+                            return <Feather name="user-plus" size={size} color={color} />;
+                        } else if (route.name === "Logout") {
+                            return <SimpleLineIcons name="logout" size={size} color={color} />;
+                        }
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: "tomato",
+                    inactiveTintColor: "gray",
+                }}>
+                <Tab.Screen name="Blogs" component={Blogs} initialParams={{ message: "" }} />
+                <Tab.Screen name="Post_Blog" component={PostBlog} />
+                <Tab.Screen name="Sign_Up" component={Sign_Up} />
+                <Tab.Screen name="Login" component={Login} />
+                <Tab.Screen name="Logout" component={Logout} />
+                <Tab.Screen name="Details" component={Blog_Info} />
+            </Tab.Navigator>
+            <StatusBar style="dark" />
+            <Toast ref={(ref) => Toast.setRef(ref)} />
+        </NavigationContainer>
     );
 }
